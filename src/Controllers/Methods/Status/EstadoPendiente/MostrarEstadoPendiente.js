e7d34958e85b828d2d96ea3710ab7adac07a98b3
Manuel Cruz <Jose.Cruz@grow-analytics.com.pe>
2023-05-30 17:52:19 -0500

    Api Status

--- src/Controllers/Methods/Status/EstadoPendiente/MostrarEstadoPendiente.js
+++ src/Controllers/Methods/Status/EstadoPendiente/MostrarEstadoPendiente.js
@@ -16,8 +16,9 @@ controller.MetMostrarEstadoPendiente = async ( req, res ) => {
         let date_two    = ""
         let data        = []
         let esps_dts    = []
+        let query_eps   = {}
 
-        if(date_final){
+        if(date_final != null){
             date_final = moment(date_final).format("YYYY-MM");
         }else{
             date_final = null
@@ -25,61 +26,188 @@ controller.MetMostrarEstadoPendiente = async ( req, res ) => {
 
         const tprs  = await prisma.tprtipospromociones.findMany({})
 
-        let index = 0
+        let index_tpr = 0
+
+        if(date_final == null){
+            query_eps = {...query_eps, espid : 0}
+        }else{
+            query_eps = {...query_eps, fecfecha: date_final+'-01'}
+        }
 
         for await (const tpr of tprs ){
-            console.log('------------TPDR--' + tpr.tprid)
 
-            const ares = await prisma.espestadospendientes.findMany({
-                include : {
+            let ares = await prisma.espestadospendientes.findMany({
+                select : {
                     areareasestados : {
-                        include : {
+                        select : {
+                            areid               : true,
+                            areicono            : true,
+                            arenombre           : true,
+                            areporcentaje       : true,
                             tprtipospromociones : true
-                        }
+                        },
                     },
-                    fecfechas : true
+                    fecfechas : true,
                     
                 },
+                // where : query_eps,
+                distinct: ['areid'],
             })
 
-            // if(index == 0){
-            //     console.log(ares)
-            //     console.log(ares.tprtipospromociones)
-            // }
-            // if(ares.areareasestados.tprtipospromociones){
-            //     if(ares.areareasestados.tprtipospromociones.tprid == tpr.tprid){
-            //         data.push(ares)
-            //     }
-            // }
-            let data_pre = ares.filter(dat => dat.areareasestados.tprtipospromociones.tprid == tpr.tprid)
-            data.push(data_pre)
-
-            console.log('------------------------------------------')
-
-
+            ares = ares.filter(are => are.areareasestados.tprtipospromociones.tprid == tpr.tprid)
 
             if(ares.length > 0){
+
                 let aresn = [ [], [], [], [], [] ]
 
-                let date_actual = moment().format('YYYY-MM-DD');
-                date_one        = moment(date_actual)    
-
                 let index_ares = 0
-                for await (const tpr of ares ){
+
+                for await (const are of ares ){
                     
                     const esps = await prisma.espestadospendientes.findMany({
-
+                        where : {
+                            areid : are.areareasestados.areid
+                        },
+                        select : {
+                            perpersonas : {
+                                select : {
+                                    pernombrecompleto   : true,
+                                    pernombre           : true,
+                                    perapellidopaterno  : true,
+                                    perapellidomaterno  : true,
+                                }
+                            },
+                            espid               : true,
+                            espfechaprogramado  : true,
+                            espchacargareal     : true,
+                            espfechactualizacion: true,
+                            espbasedato         : true,
+                            espresponsable      : true,
+                            espdiaretraso       : true,
+                        }
                     })
+
+                    esps.forEach((esp, index_esp)=> {
+
+                        let day_late = esp.espdiaretraso
+
+                        if(esp.espfechactualizacion == null){
+
+                            date_two    = moment(esp.espfechaprogramado)
+                            date_one    = moment()
+                            
+                            if(date_one > date_two){
+                                let diff_days_date_one_two = date_one.diff(date_two, 'days')
+                                
+                                if( diff_days_date_one_two > 0){
+                                    day_late = diff_days_date_one_two
+                                }else{
+                                    day_late = '0'
+                                }
+                            }else{
+                                day_late = '0'
+                            }
+                        }
+
+                        esps[index_esp]['espdiaretraso'] = day_late
+                    })
+
+                    ares[index_ares]['esps'] = esps
+
+                    if(are.areareasestados.arenombre == 'SAC Sell Out Detalle'){
+                        aresn[3]    = ares[index_ares]
+                    }else if(are.areareasestados.arenombre == 'SAC Sell Out'){
+                        aresn[2]    = ares[index_ares]
+                    }else if(are.areareasestados.arenombre == 'SAC Sell In'){
+                        aresn[1]    = ares[index_ares]
+                    }else if(are.areareasestados.arenombre == 'Revenue'){
+                        aresn[0]    = ares[index_ares]
+                    }else if(are.areareasestados.arenombre == 'SAC ADM'){
+                        aresn[4]    = ares[index_ares]
+                    }else{
+                        aresn[5]    = ares[index_ares]
+                    }
+
+                    index_ares = index_ares + 1
+
                 }
+
+                tprs[index_tpr]['ares'] = aresn
+
+                if(index_tpr == 0){
+                    tprs[index_tpr]['seleccionado'] = true
+                }
+
+                esps_dts = await prisma.espestadospendientes.findMany({
+                    select : {
+                        perpersonas : true,
+                        areareasestados : {
+                            select : {
+                                tprtipospromociones : true,
+                                arenombre : true
+                            }
+                        },
+                        espdiaretraso : true,
+                        espfechactualizacion : true,
+                        espfechaprogramado : true,
+                        fecfechas : true,
+                        cliclientes : {
+                            select : {
+                                zonzonas : true
+                            }
+                        }
+                    },
+                })
+
+                esps_dts = esps_dts.filter(esp => 
+                                            esp.areareasestados.tprtipospromociones.tprid == tpr.tprid 
+                                            && esp.areareasestados.arenombre == 'SAC Sell Out Detalle' 
+                                            && esp.fecfechas.fecfecha == date_final+'-01'
+                                        )
+
+                esps_dts.forEach( (esp_dts, index_esp_dts) => {
+                    
+                    let day_late = esp_dts.espdiaretraso
+
+                    if(esp_dts.espfechactualizacion == null){
+
+                        date_one    = moment()
+                        date_two    = moment(esp_dts.espfechaprogramado)
+
+                        if(date_one == date_two){
+                            day_late = '0'
+                        }else{
+                            if(date_one > date_two){
+                                let diff_days_date_one_two = date_one.diff(date_two, 'days')
+                                
+                                if( diff_days_date_one_two > 0){
+                                    day_late = diff_days_date_one_two
+                                }else{
+                                    day_late = '0'
+                                }
+                            }else{
+                                day_late = '0'
+                            }
+                        }
+                    }
+
+                    esps_dts[index_esp_dts]['espdiaretraso'] = day_late
+                });
+
             }
 
-            index = index + 1
+            index_tpr = index_tpr + 1
         }
+
+        data = tprs
 
         res.status(200).json({
             response    : true,
             messagge    : 'Se obtuvo el estado pendiente de status con éxito',
-            data        : data
+            date1       : date_one, 
+            date2       : date_two,
+            datos       : data,
+            espsDistribuidoras : esps_dts
         })
 
     }catch(err){
