2fcacd9370f642ccf985003651c2dae2183db83c
Gerson <gerson.vilca@grow-analytics.com.pe>
2023-07-06 16:21:04 -0500

    stat

--- src/Controllers/Methods/Status/EstadoPendiente/MostrarEstadoPendiente.js
+++ src/Controllers/Methods/Status/EstadoPendiente/MostrarEstadoPendiente.js
@@ -185,7 +185,7 @@ controller.MetMostrarEstadoPendiente = async ( req, res=null ) => {
     
                     if(are.arenombre == 'SAC'){
                         are.areporcentaje                   = prod_no_hml_count == 0 ? '100' : '0'
-                        are.esps[0]['perpersonas']          = last_prod_hml.usuusuarios ? last_prod_hml.usuusuarios.perpersonas : null
+                        are.esps[0]['perpersonas']          = last_prod_hml.usuusuarios ? last_prod_hml?.usuusuarios?.perpersonas : null
                         are.esps[0]['espfechactualizacion'] = last_prod_hml?.updated_at
                         are.esps[0]['espdiaretraso']        = prod_no_hml_count == 0 ? '0' : day_late_sac
                         areid_sac = are.areid
